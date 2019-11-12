e684d2d3a3af9c30d8f59c3020a601920eaa9a68
jlsajfj <jlsajfj@hotmail.com>
2019-11-12 05:07:05 -0500

    useless input removed

--- tic tac toe.py
+++ tic tac toe.py
@@ -17,6 +17,3 @@ while not won:
   print (lambda ch: any(map(lambda a: all(map(lambda __: False if arr[a[0]*3+__] != a[1] else True, [0,1,2])),([i,ch] for i in range(3)))))('o')
 
   print "\n".join(map(lambda z: "".join(arr[z*3:(z+1)*3]), [0,1,2]))
-
-(lambda f: (lambda x: f(lambda v: x(x)(v)))(lambda x: f(lambda v: x(x)(v))))(lambda f: (lambda a: (__import__("operator").setitem(arr,(a[0]-1)*3+a[1]-1,'x')) if (a[0] in [1,2,3] and a[1] in [1,2,3] and arr[(a[0]-1)*3+a[1]-1] == '.') else f(map(int,raw_input().split()))))(map(int,raw_input().split()))
-print "\n".join(map(lambda z: "".join(arr[z*3:(z+1)*3]), [0,1,2]))
