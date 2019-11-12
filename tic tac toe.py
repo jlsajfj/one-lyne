459168e2a9b29695d4ecd88645c1dba346630b7c
jlsajfj <jlsajfj@hotmail.com>
2019-11-12 05:14:33 -0500

    Lambda compress of fullness

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,14 +1,8 @@
 arr = ['.']*9
 
-for _ in range(9):
-  (lambda f: (lambda x: f(lambda v: x(x)(v)))(lambda x: f(lambda v: x(x)(v))))(lambda f: (lambda a: (__import__("operator").setitem(arr,(a[0]-1)*3+a[1]-1,'o')) if (a[0] in [1,2,3] and a[1] in [1,2,3] and arr[(a[0]-1)*3+a[1]-1] == '.') else f([__import__("random").randint(1,3),__import__("random").randint(1,3)])))([__import__("random").randint(1,3),__import__("random").randint(1,3)])
 
-print "\n".join(map(lambda z: "".join(arr[z*3:(z+1)*3]), [0,1,2]))
+print not any(map(lambda c: c=='.', arr))
 
-def toBool(c):
-  return c=='.'
-aConv = map(toBool, arr)
-print not any(aConv)
 
 won = True
 while not won:
