515b92084517fc0092458f873d4b016468cb2cd2
jlsajfj <jlsajfj@hotmail.com>
2019-11-12 05:13:53 -0500

    Testing the full conditional

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,12 +1,16 @@
 arr = ['.']*9
 
+for _ in range(9):
+  (lambda f: (lambda x: f(lambda v: x(x)(v)))(lambda x: f(lambda v: x(x)(v))))(lambda f: (lambda a: (__import__("operator").setitem(arr,(a[0]-1)*3+a[1]-1,'o')) if (a[0] in [1,2,3] and a[1] in [1,2,3] and arr[(a[0]-1)*3+a[1]-1] == '.') else f([__import__("random").randint(1,3),__import__("random").randint(1,3)])))([__import__("random").randint(1,3),__import__("random").randint(1,3)])
+
+print "\n".join(map(lambda z: "".join(arr[z*3:(z+1)*3]), [0,1,2]))
 
 def toBool(c):
   return c=='.'
 aConv = map(toBool, arr)
+print not any(aConv)
 
-
-won = False
+won = True
 while not won:
   #player input with verification
   (lambda f: (lambda x: f(lambda v: x(x)(v)))(lambda x: f(lambda v: x(x)(v))))(lambda f: (lambda a: (__import__("operator").setitem(arr,(a[0]-1)*3+a[1]-1,'x')) if (a[0] in [1,2,3] and a[1] in [1,2,3] and arr[(a[0]-1)*3+a[1]-1] == '.') else f(map(int,raw_input().split()))))(map(int,raw_input().split()))
@@ -14,6 +18,11 @@ while not won:
   print (lambda ch: any(map(lambda a: all(map(lambda __: False if arr[a[0]*3+__] != a[1] else True, [0,1,2])),([i,ch] for i in range(3)))))('x')
 
   print "\n".join(map(lambda z: "".join(arr[z*3:(z+1)*3]), [0,1,2]))
+  
+  def toBool(c):
+    return c=='.'
+  aConv = map(toBool, arr)
+  print not any(aConv)
 
 
   #random input
@@ -22,3 +31,8 @@ while not won:
   print (lambda ch: any(map(lambda a: all(map(lambda __: False if arr[a[0]*3+__] != a[1] else True, [0,1,2])),([i,ch] for i in range(3)))))('o')
 
   print "\n".join(map(lambda z: "".join(arr[z*3:(z+1)*3]), [0,1,2]))
+  
+  def toBool(c):
+    return c=='.'
+  aConv = map(toBool, arr)
+  print not any(aConv)
