489e818cf6ef0491ee790f61dfd6b6fe356eeb16
jlsajfj <jlsajfj@hotmail.com>
2019-11-12 05:59:54 -0500

    conversion from various random snippets into condition

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,19 +1,13 @@
 arr = ['.']*9
 t = [0,1,2]
 var = 1
-won = True
+won = False
 
 
-while not won:
+while not((lambda ch: any(map(lambda a: all(map(lambda __: False if arr[a[0]*3+__] != a[1] else True, t)),([i,ch] for i in t))))('x' if var == -1 else 'o') or (lambda ch: any(map(lambda a: all(map(lambda __: False if arr[__*3+a[0]] != a[1] else True, t)),([i,ch] for i in t))))('x' if var == -1 else 'o') or (lambda ch: all(map(lambda _: arr[_*4] == ch, t)))('x' if var == -1 else 'o') or (lambda ch: all(map(lambda _: arr[_*2+2] == ch, t)))('x' if var == -1 else 'o') or not any(map(lambda c: c=='.', arr))):
   var = var * -1
   #input with verification
   (lambda f: (lambda x: f(lambda v: x(x)(v)))(lambda x: f(lambda v: x(x)(v))))(lambda f: (lambda a: (__import__("operator").setitem(arr,(a[0]-1)*3+a[1]-1,'x' if var == -1 else 'o')) if (a[0]-1 in t and a[1]-1 in t and arr[(a[0]-1)*3+a[1]-1] == '.') else f(map(int,raw_input().split()) if var==-1 else [__import__("random").randint(1,3),__import__("random").randint(1,3)])))(map(int,raw_input().split()) if var==-1 else [__import__("random").randint(1,3),__import__("random").randint(1,3)])
-
-  print (lambda ch: any(map(lambda a: all(map(lambda __: False if arr[a[0]*3+__] != a[1] else True, t)),([i,ch] for i in t))))('x' if var == -1 else 'o')
-  print (lambda ch: any(map(lambda a: all(map(lambda __: False if arr[__*3+a[0]] != a[1] else True, t)),([i,ch] for i in t))))('x' if var == -1 else 'o')
-  print (lambda ch: all(map(lambda _: arr[_*4] == ch, t)))('x' if var == -1 else 'o')
-  print (lambda ch: all(map(lambda _: arr[_*2+2] == ch, t)))('x' if var == -1 else 'o')
-  print not any(map(lambda c: c=='.', arr))
 
 
   print "\n".join(map(lambda z: "".join(arr[z*3:(z+1)*3]), t))
