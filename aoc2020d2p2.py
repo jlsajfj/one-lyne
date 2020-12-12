d111e8660752de54c552c30bce797c6113510ab0
jlsajfj <jlsajfj@hotmail.com>
2020-12-11 20:37:01 -0500

    Create aoc2020d2p2.py

---
+++ aoc2020d2p2.py
@@ -0,0 +1,1 @@
+print(len(list(filter(lambda ps: (lambda p: (lambda a,b: ((a and not b) or (b and not a)))(ps[1][p[0]-1]==ps[0][1],ps[1][p[1]-1]==ps[0][1]))(list(map(int,ps[0][0].split('-')))),list(map((lambda x: [x[0].split(' '),x[1].strip()]),list(map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))))))))
